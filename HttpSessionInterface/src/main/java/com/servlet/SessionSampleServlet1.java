485129635ce6445d29bfcadae2199ef66b51e576
Vishal S Jadhav <100112614+VishalSJ97@users.noreply.github.com>
2022-12-03 12:32:57 -0500

    updated the explaination

--- HttpSessionInterface/src/main/java/com/servlet/SessionSampleServlet1.java
+++ HttpSessionInterface/src/main/java/com/servlet/SessionSampleServlet1.java
@@ -10,6 +10,33 @@ import jakarta.servlet.http.HttpSession;
 public class SessionSampleServlet1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * In such case, container creates a session id for each user.
+	 * The container uses this id to identify the particular user.
+	 * An object of HttpSession can be used to perform two tasks:
+	 * 1. bind objects
+	 * 2. view and manipulate information about a session, such as the session identifier, creation time, and last accessed time.
+	 * 
+	 * 
+	 * getting HTTPSession object
+	 * public HttpSession getSession():
+	 * 	Returns the current session associated with this request, or if the request does not have a session, creates one.
+	 * public HttpSession getSession(boolean create):
+	 * 	Returns the current HttpSession associated with this request or, if there is no current session and create is true,
+	 * 	returns a new session.
+	 * 
+	 * Commonly used methods of http Session interface
+	 * public String getId():
+	 * 	Returns a string containing the unique identifier value.
+	 * public long getCreationTime():
+	 * 	Returns the time when this session was created, measured in milliseconds since midnight January 1, 1970 GMT.
+	 * public long getLastAccessedTime():
+	 * 	Returns the last time the client sent a request associated with this session,
+	 * 	as the number of milliseconds since midnight January 1, 1970 GMT.
+	 * public void invalidate():
+	 * 	Invalidates this session then unbinds any objects bound to it.
+	 */
+	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 	{  
 		try
